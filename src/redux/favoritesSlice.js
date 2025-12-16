9016f88100603802795068c6aecf795fa35f89f5
Rommel Dijon <bartmilhausky@gmail.com>
2025-12-16 17:01:12 +1300

    Implement Redux favorites slice logic to toggle recipes by idFood

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -1,16 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  favoriterecipes: [], // Updated to handle favorite articles
+  favoriterecipes: [],
 };
 
 const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
-   
+    toggleFavorite: (state, action) => {
+      const recipe = action.payload;
+
+      const index = state.favoriterecipes.findIndex(
+        (item) => item.idFood === recipe.idFood
+      );
+
+      if (index !== -1) {
+        // If recipe already exists, remove it
+        state.favoriterecipes.splice(index, 1);
+      } else {
+        // If recipe does not exist, add it
+        state.favoriterecipes.push(recipe);
+      }
+    },
   },
 });
 
 export const { toggleFavorite } = favoritesSlice.actions;
 export default favoritesSlice.reducer;
+
